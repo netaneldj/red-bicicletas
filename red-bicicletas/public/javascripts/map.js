6fb6c908d64ded2bb675d9f7bd3a3ebb9a98e749
Netanel <njamilis@fi.uba.ar>
2020-09-17 12:22:08 -0300

    API

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -4,6 +4,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy: <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
 }).addTo(map);
 
-L.marker([-34.605902, -58.435716]).addTo(map)
-L.marker([-34.568351, -58.417783]).addTo(map)
-L.marker([-34.609848, -58.351524]).addTo(map)
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
