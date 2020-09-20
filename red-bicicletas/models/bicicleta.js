f9a20ff4f5fd53b4208aacb9c36141eae6277469
Netanel <njamilis@fi.uba.ar>
2020-09-20 19:58:44 -0300

    Agrego testing jasmine

--- red-bicicletas/models/bicicleta.js
+++ red-bicicletas/models/bicicleta.js
@@ -33,10 +33,10 @@ Bicicleta.removeById = function(aBiciId) {
     }
 }
 
-var a = new Bicicleta(1, 'rojo', 'urbana', [-34.584595, -58.408742]);
+/*var a = new Bicicleta(1, 'rojo', 'urbana', [-34.584595, -58.408742]);
 var b = new Bicicleta(2, 'blanca', 'urbana', [-34.582533, -58.417165]);
 
 Bicicleta.add(a);
-Bicicleta.add(b);
+Bicicleta.add(b);*/
 
 module.exports = Bicicleta;
