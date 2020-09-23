3df9290e43de7fe70b191ca7fbfb41d22c51e94d
Netanel <njamilis@fi.uba.ar>
2020-09-23 13:48:18 -0300

    Facebook auth

--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -4,7 +4,7 @@ var bicicletaController = require('../../controllers/api/bicicletaControllerAPI'
 
 router.get('/', bicicletaController.bicicleta_list);
 router.post('/create', bicicletaController.bicicleta_create);
-router.put('/:id/update',bicicletaControllerApi.bicicleta_update_post)
+router.put('/:id/update', bicicletaControllerApi.bicicleta_update_post)
 router.delete('/:id/delete', bicicletaControllerApi.bicicleta_delete)
 
 module.exports = router;
